e136310814b3a287a67c7ef7773b506dd24b558c
Valenargaezceballos02 <142552161+Valenargaezceballos02@users.noreply.github.com>
2024-03-04 22:25:26 -0500

    Add files via upload

---
+++ CitasMedicas/Entidades/ClsMedicos.cs
@@ -0,0 +1,119 @@
+﻿using AccesoDatos.DataBase;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    internal class ClsMedicosLn
+    {
+        #region Variables Privadas
+        private ClsDataBase ObjDataBase = null;
+        #endregion
+
+        #region Metodo Index
+        public void Index(ref Entidades.ClsMedicosLn objMedicos)
+        {
+            ObjDataBase = new ClsDataBase()
+            {
+                NombreTabla = "tbl_medicos",
+                NombreSP = "[dbo].[SP_Medicos_Index]",
+                Scalar = false,
+            };
+            Ejecutar(ref objMedicos);
+        }
+        private void Ejecutar(ref Entidades.ClsMedicosLn objMedicos)
+        {
+            ObjDataBase.CRUD(ref ObjDataBase);
+            if (ObjDataBase.MensajeErrorOS == null) //Si es null no hay error
+            {
+                if (ObjDataBase.Scalar)
+                {
+                    objMedicos.ValorScalar = ObjDataBase.ValorScalar; //Tipo valor escalar
+                }
+                else
+                {
+                    objMedicos.DtResultado = ObjDataBase.DsResultados.Tables[0];
+                    //Validar si la tabla tiene un solo registro
+                    if (objMedicos.DtResultado.Rows.Count == 1)
+                    {
+                        foreach (DataRow item in objMedicos.DtResultado.Rows)
+                        {
+                            objMedicos.CedulaM = item["cedulaM"].ToString();
+                            objMedicos.NombreM = item["nombreM"].ToString();
+                            objMedicos.Email = item["email"].ToString();
+                            objMedicos.Especializacion = item["especializacion"].ToString();
+
+                        }
+                    }
+                }
+            }
+            else
+            {
+                objMedicos.MensajeError = ObjDataBase.MensajeErrorOS;
+            }
+        }
+        #region CRUD Medico
+
+        public void Create(ref Entidades.ClsMedicosLn objMedicos)
+        {
+            ObjDataBase = new ClsDataBase()
+            {
+                NombreTabla = "tbl_medicos",
+                NombreSP = "[dbo].[SP_Medicos_Create]",
+                Scalar = true,
+            };
+            ObjDataBase.DtParametros.Rows.Add(@"@CedulaM", "15", objMedicos.CedulaM);
+            ObjDataBase.DtParametros.Rows.Add(@"@NombreM", "15", objMedicos.NombreM);
+            ObjDataBase.DtParametros.Rows.Add(@"@Email", "15", objMedicos.Email);
+            ObjDataBase.DtParametros.Rows.Add(@"@Especializacion", "15", objMedicos.Especializacion);
+            Ejecutar(ref objMedicos);
+        }
+        public void Update(ref Entidades.ClsMedicosLn objMedicos)
+        {
+            ObjDataBase = new ClsDataBase()
+            {
+                NombreTabla = "tbl_medicos",
+                NombreSP = "[dbo].[SP_Medicos_Update]",
+                Scalar = true,
+            };
+            ObjDataBase.DtParametros.Rows.Add(@"@CedulaM", "15", objMedicos.CedulaP);
+            ObjDataBase.DtParametros.Rows.Add(@"@NombreM", "15", objMedicos.NombreP);
+            ObjDataBase.DtParametros.Rows.Add(@"@Email", "15", objMedicos.Email);
+            ObjDataBase.DtParametros.Rows.Add(@"@Especializacion", "15", objMedicos.Especializacion);
+            Ejecutar(ref objMedicos);
+        }
+        public void Delete(ref Entidades.ClsMedicosLn objMedicos)
+        {
+            ObjDataBase = new ClsDataBase()
+            {
+                NombreTabla = "tbl_medicos",
+                NombreSP = "[dbo].[SP_Medicos_Delete]",
+                Scalar = true,
+            };
+            ObjDataBase.DtParametros.Rows.Add(@"@CedulaM", "15", objMedicos.CedulaM);
+            Ejecutar(ref objMedicos);
+        }
+        public void Read(ref Entidades.ClsMedicosLn objMedicos)
+        {
+            ObjDataBase = new ClsDataBase()
+            {
+                NombreTabla = "tbl_medicos",
+                NombreSP = "[dbo].[SP_Medicos_Read]",
+                Scalar = false,
+            };
+            ObjDataBase.DtParametros.Rows.Add(@"@CedulaM", "15", objMedicos.CedulaM);
+            Ejecutar(ref objMedicos);
+        }
+
+        #endregion
+
+        #region Metodos Privados
+
+        #endregion
+    }
+}
---
+++ CitasMedicas/Presentacion/Pacientes.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Presentacion
+{
+    partial class Pacientes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(84, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(792, 285);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cédula";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(454, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Correo electrónico:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(457, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "EPS:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(275, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(457, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(398, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(457, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(260, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Pacientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 520);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Pacientes";
+            this.Text = "FrmPacientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
